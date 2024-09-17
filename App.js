32383326772ad8e9eb7328d1e2e9543618c422bb
abayram <38274063+abdullahbayram@users.noreply.github.com>
2024-09-17 18:01:03 +0200

    chore: install and configure Jest, update ESLint config for testing environment
    - Installed and configured Jest with related libraries
    - Added Jest-specific ESLint configurations for test environments
    - Integrated @testing-library/react-native with Jest setup

--- App.js
+++ App.js
@@ -3,12 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-      <View style={styles.container}>
-      <Text >   Open up App.js to start working on your app!<
-      /Text>
-        {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto"
-      />
+    <View style={styles.container}>
+      <Text> Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <StatusBar style="auto" />
     </View>
   );
 }
