79cb23e81845cbfaa7ef475bdab5c41976c3926c
abayram <38274063+abdullahbayram@users.noreply.github.com>
2024-10-05 23:58:04 +0200

    chore: add Husky pre-push hook to enforce validation before pushing
    - Set up Husky pre-push hook to run linting and tests
    - Ensure code meets quality standards before pushing to remote

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,5 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|react-redux)',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coverageThreshold: { global: { branches: 90, functions: 90, lines: 90, statements: 90 } },
 };
