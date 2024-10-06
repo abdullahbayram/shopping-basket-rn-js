14c1244380e82d9ac3257b6c366ca3ca87d568f3
abayram <38274063+abdullahbayram@users.noreply.github.com>
2024-10-06 14:06:53 +0200

    test(msw): update handlers and use mock server in tests
    - Fixed MSW handlers to return correct mocked product data.
    - Uncommented MSW server setup in jest for API mocking before tests.

--- __tests__/mocks/handlers.js
+++ __tests__/mocks/handlers.js
@@ -1,46 +1,43 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { http, httpResponse } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
   // Mock the /products API endpoint
-  http.get('http://localhost:9001/products', (req, res, ctx) => {
+  http.get('http://localhost:9001/products', () => {
     console.warn('WELCOME');
-    return httpResponse.json(
-      ctx.status(200),
-      ctx.json([
-        {
-          sku: 1,
-          name: 'Product One',
-          description: 'Product One description',
-          price: 1.11,
-        },
-        {
-          sku: 2,
-          name: 'Product Two',
-          description: 'Product Two description',
-          price: 2.22,
-        },
-        {
-          sku: 3,
-          name: 'Product Three',
-          description: 'Product Three description',
-          price: 3.33,
-        },
-        {
-          sku: 4,
-          name: 'Product Four',
-          description: 'Product Four description',
-          price: 4.44,
-        },
-        {
-          sku: 5,
-          name: 'Product Five',
-          description: 'Product Five description',
-          price: 5.55,
-        },
-      ]),
-    );
+    return HttpResponse.json([
+      {
+        sku: 1,
+        name: 'Product One',
+        description: 'Product One description',
+        price: 1.11,
+      },
+      {
+        sku: 2,
+        name: 'Product Two',
+        description: 'Product Two description',
+        price: 2.22,
+      },
+      {
+        sku: 3,
+        name: 'Product Three',
+        description: 'Product Three description',
+        price: 3.33,
+      },
+      {
+        sku: 4,
+        name: 'Product Four',
+        description: 'Product Four description',
+        price: 4.44,
+      },
+      {
+        sku: 5,
+        name: 'Product Five',
+        description: 'Product Five description',
+        price: 5.55,
+      },
+    ]);
   }),
 
   // Mock the /checkout API endpoint
--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseUrl = 'http://localhost:9001/';
+
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9001/' }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => 'products',
